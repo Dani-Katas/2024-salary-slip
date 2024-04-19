252795ec4b5b3a1192d512a217189ce1287b3f64
Miguel Ángel Pérez García <miguelangel.perez.dev@gmail.com>
2024-04-19 16:16:15 +0100

    ref: add value object Money

--- src/Employee.ts
+++ src/Employee.ts
@@ -1,20 +1,29 @@
 import { SalarySlip } from "./SalarySlip.js"
+import { Money } from "./Money.js"
 
 export class Employee {
+  private grossSalary: Money
+
   constructor(
     private readonly id: number,
     private readonly name: string,
-    private readonly grossSalary: number,
-  ) {}
+    grossSalary: number,
+  ) {
+    this.grossSalary = new Money(grossSalary)
+  }
 
   getSalarySlip(): SalarySlip {
-    const result = this.grossSalary / 12
-    const formatted = "£" + result.toFixed(2)
+    const formatted = this.formattedMonthlyGrossSalary()
 
     return {
       employeeId: this.id,
       employeeName: this.name,
       monthlyGrossSalary: formatted,
     }
+  }
+
+  private formattedMonthlyGrossSalary() {
+    const monthlyGrossSalary = this.grossSalary.divideBy(12)
+    return monthlyGrossSalary.toString()
   }
 }
---
+++ src/Money.ts
@@ -0,0 +1,15 @@
+export class Money {
+  constructor(private readonly value: number) {}
+
+  getValue(): number {
+    return this.value
+  }
+
+  public toString(): string {
+    return "£" + this.value.toFixed(2)
+  }
+
+  public divideBy(divider: number): Money {
+    return new Money(this.value / 12)
+  }
+}

9ceecd34f960ae9196cda578de2d2b0bb4a8eb52
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-19 16:57:52 +0200

    remove feature envy

--- src/Employee.ts
+++ src/Employee.ts
@@ -1,3 +1,5 @@
+import { SalarySlip } from "./SalarySlip.js"
+
 export class Employee {
   constructor(
     private readonly id: number,
@@ -5,15 +7,11 @@ export class Employee {
     private readonly grossSalary: number,
   ) {}
 
-  getId() {
-    return this.id
-  }
-
-  getName() {
-    return this.name
-  }
-
-  getGrossSalary() {
-    return this.grossSalary
+  getSalarySlip(): SalarySlip {
+    return {
+      employeeId: this.id,
+      employeeName: this.name,
+      monthlyGrossSalary: this.grossSalary === 120 ? "£10" : "£416.67",
+    }
   }
 }
--- src/SalarySlipGenerator.ts
+++ src/SalarySlipGenerator.ts
@@ -3,10 +3,6 @@ import { SalarySlip } from "./SalarySlip.js"
 
 export class SalarySlipGenerator {
   generateFor(employee: Employee): SalarySlip {
-    return {
-      employeeId: employee.getId(),
-      employeeName: employee.getName(),
-      monthlyGrossSalary: employee.getGrossSalary() === 120 ? "£10" : "£416.67",
-    }
+    return employee.getSalarySlip()
   }
 }

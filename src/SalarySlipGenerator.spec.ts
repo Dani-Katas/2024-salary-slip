bbd090cd526e79b95a88796e2ad59fbcc6385459
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-07 16:38:25 +0200

    red: employee id

--- src/SalarySlipGenerator.spec.ts
+++ src/SalarySlipGenerator.spec.ts
@@ -2,8 +2,18 @@ import { describe, it, expect } from "vitest"
 
 describe("SalarySlipGenerator", () => {
   describe("generates the salary slip with the", () => {
-    it("employee id", () => {})
+    it("employee id", () => {
+      const employeeId = "12345"
+      const employee = new Employee(employeeId, "John J Doe", 5000)
+
+      const salarySlip = salarySlipGenerator.generateFor(employee)
+
+      const id = salarySlip.getId()
+      expect(id).toHaveProperty(employeeId)
+    })
+
     it.todo("employee name")
+
     it.todo("monthly gross salary")
   })
 })

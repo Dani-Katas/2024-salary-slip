b12b06bb7b593170110ee2cfe05c6a37ad9a3f7a
Pablo400-glitch <alu0101311135@ull.edu.es>
2024-06-07 15:47:12 +0100

    green: employee id

--- src/SalarySlipGenerator.spec.ts
+++ src/SalarySlipGenerator.spec.ts
@@ -1,18 +1,28 @@
 import { describe, it, expect } from "vitest"
 
 class Employee {
-  constructor(id: string, name: string, anualGrossSalary: number) {}
+  constructor(
+    private id: string,
+    name: string,
+    anualGrossSalary: number,
+  ) {}
+
+  getId() {
+    return this.id
+  }
 }
 
 class SalarySlip {
+  constructor(private employee: Employee) {}
+
   getId() {
-    return "12345"
+    return this.employee.getId()
   }
 }
 
 class SalarySlipGenerator {
   generateFor(employee: Employee): SalarySlip {
-    return new SalarySlip()
+    return new SalarySlip(employee)
   }
 }
 
@@ -20,17 +30,6 @@ describe("SalarySlipGenerator", () => {
   describe("generates the salary slip with the", () => {
     it("employee id", () => {
       const employeeId = "12345"
-      const employee = new Employee(employeeId, "John J Doe", 5000)
-      const salarySlipGenerator = new SalarySlipGenerator()
-
-      const salarySlip = salarySlipGenerator.generateFor(employee)
-
-      const id = salarySlip.getId()
-      expect(id).toBe(employeeId)
-    })
-
-    it("employee id2", () => {
-      const employeeId = "123"
       const employee = new Employee(employeeId, "John J Doe", 5000)
       const salarySlipGenerator = new SalarySlipGenerator()
 

c146ddeb71125c1402e53d0e7abf113018bc6272
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-07 16:40:30 +0200

    green: employee id

--- src/SalarySlipGenerator.spec.ts
+++ src/SalarySlipGenerator.spec.ts
@@ -1,15 +1,32 @@
 import { describe, it, expect } from "vitest"
 
+class Employee {
+  constructor(id: string, name: string, anualGrossSalary: number) {}
+}
+
+class SalarySlip {
+  getId() {
+    return "12345"
+  }
+}
+
+class SalarySlipGenerator {
+  generateFor(employee: Employee): SalarySlip {
+    return new SalarySlip()
+  }
+}
+
 describe("SalarySlipGenerator", () => {
   describe("generates the salary slip with the", () => {
     it("employee id", () => {
       const employeeId = "12345"
       const employee = new Employee(employeeId, "John J Doe", 5000)
+      const salarySlipGenerator = new SalarySlipGenerator()
 
       const salarySlip = salarySlipGenerator.generateFor(employee)
 
       const id = salarySlip.getId()
-      expect(id).toHaveProperty(employeeId)
+      expect(id).toBe(employeeId)
     })
 
     it.todo("employee name")

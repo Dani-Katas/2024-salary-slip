057355a20d594247be38ab250166dcf92e15af31
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-07 16:35:51 +0200

    draft first iteration tests

---
+++ src/SalarySlipGenerator.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest"
+
+describe("SalarySlipGenerator", () => {
+  describe("generates the salary slip with the", () => {
+    it("employee id", () => {})
+    it.todo("employee name")
+    it.todo("monthly gross salary")
+  })
+})
